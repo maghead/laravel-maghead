bc4034c249967d9c3ef0b53946a0bcef0bde4a0b
recca0120 <recca0120@gmail.com>
2017-05-16 22:58:39 +0800

    demo data

--- config/maghead.php
+++ config/maghead.php
@@ -1,3 +1,37 @@
 <?php
 
-return [];
+return [
+  'cli' => [
+    'bootstrap' => 'tests/bootstrap.php',
+  ],
+  'schema'=> [
+    'auto_id'=> true
+    'base_model'=> \Maghead\Runtime\BaseModel::class
+    'base_collection'=> \Maghead\Runtime\BaseCollection::class
+    'paths'=> [
+        'tests'
+    ]
+  ],
+  'instance' => [
+    'local' => [
+      'dsn' => 'mysql:host=localhost',
+      'user' => 'root'
+    ],
+    'databases' => [
+      'master' => [
+        'dsn' => 'sqlite::memory:'
+        'query_options' => [
+          'quote_column'=> true,
+          'quote_table'=> true
+        ]
+      ],
+      'mysql' => [
+        'dsn' => 'mysql:host=localhost;dbname=testing'
+        'user' => 'root'
+      ],
+      'pgsql' => [
+          'dsn' => 'pgsql:host=localhost;dbname=testing'
+          'user' => postgres
+        ]
+    ]
+  ]
